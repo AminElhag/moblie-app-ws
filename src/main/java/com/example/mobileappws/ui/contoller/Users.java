1a25e3216397003a582ed0c3d15c820b47245727
Amin Elhag <aminjalalzoom@gmail.com>
2022-09-19 14:23:26 +0200

    Add exception Handling

--- src/main/java/com/example/mobileappws/ui/contoller/Users.java
+++ src/main/java/com/example/mobileappws/ui/contoller/Users.java
@@ -1,7 +1,9 @@
 package com.example.mobileappws.ui.contoller;
 
+import com.example.mobileappws.exception.UserServiceException;
 import com.example.mobileappws.service.UserService;
 import com.example.mobileappws.ui.model.request.UserDetailsRequestModel;
+import com.example.mobileappws.ui.model.resposne.ErrorMessages;
 import com.example.mobileappws.ui.model.resposne.UserRes;
 import com.example.mobileappws.ui.shared.dto.UserDto;
 import org.springframework.beans.BeanUtils;
@@ -16,10 +18,7 @@ public class Users {
     UserService userService;
 
 
-    @GetMapping(
-            path = "/{id}",
-            produces = {MediaType.APPLICATION_JSON_VALUE, MediaType.APPLICATION_XML_VALUE}
-    )
+    @GetMapping(path = "/{id}", produces = {MediaType.APPLICATION_JSON_VALUE, MediaType.APPLICATION_XML_VALUE})
     public UserRes getUser(@PathVariable String id) {
         UserRes res = new UserRes();
         UserDto userDto = userService.getUserDtoById(id);
@@ -27,18 +26,17 @@ public class Users {
         return res;
     }
 
-    @PostMapping(
-            produces = {MediaType.APPLICATION_JSON_VALUE, MediaType.APPLICATION_XML_VALUE},
-            consumes = {MediaType.APPLICATION_JSON_VALUE, MediaType.APPLICATION_XML_VALUE}
-    )
-    public UserRes createUser(@RequestBody UserDetailsRequestModel userDetailsRequestModel) {
+    @PostMapping(produces = {MediaType.APPLICATION_JSON_VALUE, MediaType.APPLICATION_XML_VALUE}, consumes = {MediaType.APPLICATION_JSON_VALUE, MediaType.APPLICATION_XML_VALUE})
+    public UserRes createUser(@RequestBody UserDetailsRequestModel userDetailsRequest) {
 
-        System.out.println(userDetailsRequestModel);
+        if (userDetailsRequest.getFirstName() == null || userDetailsRequest.getLastName() == null || userDetailsRequest.getEmail() == null || userDetailsRequest.getPassword() == null)
+            throw new UserServiceException(ErrorMessages.MISSING_REQUIRED_FILED.getErrorMessage());
+        System.out.println(userDetailsRequest);
 
         UserRes userRes = new UserRes();
 
         UserDto userDto = new UserDto();
-        BeanUtils.copyProperties(userDetailsRequestModel, userDto);
+        BeanUtils.copyProperties(userDetailsRequest, userDto);
 
         UserDto createdUser = userService.createUser(userDto);
         BeanUtils.copyProperties(createdUser, userRes);
